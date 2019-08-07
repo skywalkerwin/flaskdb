e1257c21b1b37896cd8222ed0ea041d26cf5b4e4
luke <lukehaonguyen@gmail.com>
2019-08-07 12:05:01 -0600

    heroku test

---
+++ static/main.js
@@ -0,0 +1,16 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("WordcountApp", [])
+
+    .controller("WordcountController", [
+      "$scope",
+      "$log",
+      function($scope, $log) {
+        $scope.getResults = function() {
+          $log.log("test");
+        };
+      }
+    ]);
+})();
